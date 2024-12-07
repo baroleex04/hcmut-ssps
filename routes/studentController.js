458bd19c98944f99b12acb7d7f1c22a00942cf3d
Le Nguyen Tan Bao <nguyentanbaole2004@gmail.com>
2024-12-07 10:42:09 +0700

    final

--- routes/studentController.js
+++ routes/studentController.js
@@ -5,12 +5,8 @@ router.get("/", (req, res) => {
     res.render("student/index");
 });
 
-router.get("/upload", (req, res) => {
-    res.render("student/upload");
-});
-
-router.get("/config", (req, res) => {
-    res.render("student/config");
+router.get("/print", (req, res) => {
+    res.render("student/print");
 });
 
 router.get("/buy-page", (req, res) => {
@@ -21,8 +17,12 @@ router.get("/gift-page", (req, res) => {
     res.render("student/gift-page");
 });
 
-router.get("/view-log", (req, res) => {
-    res.render("student/view-log");
+router.get("/viewlog", (req, res) => {
+    res.render("student/viewlog");
+});
+
+router.get("/report", (req, res) => {
+    res.render("student/report");
 });
 
 module.exports = router;
